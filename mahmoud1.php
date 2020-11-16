b5c1ee7202a4b332534be33519b565153d6c4091
7oota <74470374+7oota@users.noreply.github.com>
2020-11-16 10:42:02 -0800

    Add files via upload

---
+++ mahmoud1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>سيرة ذاتية </title>
+</head>
+<body bgcolor="ffaa1525">
+    <right>
+<h2>
+    بسم الله الرحمن الرحيم
+    <br>
+    
+
+<h1>محمود محمدسعد عمر جيلاني  </h1>
+    <br> 
+     <h3>كلية : الامارات للعلوم والتكنولوجيا </h3>
+     <br>
+     <h2> قسم : تقانة المعلومات </h2>
+    <br>
+      <h2> المستوي: الثالـث </h2>
+    <br>
+     <h2>الرقم الجامعي :930901  </h2>
+    <br>
+   
+</body>
+</html>
